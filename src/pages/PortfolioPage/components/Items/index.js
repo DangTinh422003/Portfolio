c9f16a1922039d5f256a4935b6b1ac34eae452da
Dang Tinh <caodangtinh04022003@gmail.com>
2023-03-01 16:22:49 +0700

    fix scroll sidebar

--- src/pages/PortfolioPage/components/Items/index.js
+++ src/pages/PortfolioPage/components/Items/index.js
@@ -6,6 +6,46 @@ function Items() {
     <div className={clsx(styles.Items)}>
       <div className="row">
         {/* render item */}
+        <div className="col-lg-4 col-6">
+          <div className={clsx(styles.item)}>
+            <img
+              src="https://images.unsplash.com/photo-1677504210148-e39caba87b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="col-lg-4 col-6">
+          <div className={clsx(styles.item)}>
+            <img
+              src="https://images.unsplash.com/photo-1677504210148-e39caba87b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="col-lg-4 col-6">
+          <div className={clsx(styles.item)}>
+            <img
+              src="https://images.unsplash.com/photo-1677504210148-e39caba87b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="col-lg-4 col-6">
+          <div className={clsx(styles.item)}>
+            <img
+              src="https://images.unsplash.com/photo-1677504210148-e39caba87b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="col-lg-4 col-6">
+          <div className={clsx(styles.item)}>
+            <img
+              src="https://images.unsplash.com/photo-1677504210148-e39caba87b4f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
+              alt=""
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
